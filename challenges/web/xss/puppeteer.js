8781dabd752795390843ae5bfcd81d838ec7f555
Lam Jun Rong <junron1@outlook.com>
2020-12-04 07:13:25 +0800

    Prevent redirects

--- challenges/web/xss/puppeteer.js
+++ challenges/web/xss/puppeteer.js
@@ -3,6 +3,13 @@ const puppeteer = require("puppeteer");
 async function visitUrl(url, cookies) {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
+  page.on('request', request => {
+    if (request.isNavigationRequest() && request.redirectChain().length !== 0) {
+      request.abort();
+    } else {
+      request.continue();
+    }
+  });
   await page.setCookie(...cookies);
   await page.goto(url);
   setTimeout(async () => {
